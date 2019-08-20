c610865ff90dd8077d37f4170ef5d16559402f40
jesusbriod <jesusbriod@hotmail.com>
2019-08-20 13:35:25 -0500

    First Commit

--- dollar_spot_exchange_rate/src/change_rate_fix.py
+++ dollar_spot_exchange_rate/src/change_rate_fix.py
@@ -2,33 +2,38 @@ import requests
 import csv
 import json
 
-#Resques to banxico's api change rate fix
-response = requests.get('https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF43718/datos', headers={
-    'Bmx-Token': '4dd474b2ab133983dd703942c7b08f114db49aae7afa6fe4e1ae1d01803ea728'
-})
+#Resquest to banxico's api change rate fix
+try:
+    response = requests.get('https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF43718/datos', 
+    headers={
+        'Bmx-Token': '4dd474b2ab133983dd703942c7b08f114db49aae7afa6fe4e1ae1d01803ea728'
+    })
 
-#Response jsonized
-change_rate_response =  response.json()
-print(change_rate_response["bmx"]["series"][0]['datos'])
-print(change_rate_response.keys())
-print(type(change_rate_response))
+    #Response jsonized
+    change_rate_response =  response.json()
+    print(change_rate_response["bmx"]["series"][0]['datos'])
+    print(change_rate_response.keys())
+    print(type(change_rate_response))
 
-print(len(change_rate_response["bmx"]["series"][0]['datos']))
+    print(len(change_rate_response["bmx"]["series"][0]['datos']))
 
-#change_rate_response_parsed = json.loads(change_rate_response)
-#print(change_rate_response_parsed)
 
-#Create the csv writer object
 
-csv_writer =  csv.writer(open("change_rate_fix.csv", "w"), delimiter=",", lineterminator='\n')
+    #Create the csv writer object
 
-count = 0
-header =['Fecha','Parasolventarobligaciones']
-csv_writer.writerow(header)
-for row_change_rate_fix in change_rate_response["bmx"]["series"][0]['datos']:
-    row = [row_change_rate_fix['fecha'],row_change_rate_fix['dato']]
-    print(row)
-    csv_writer.writerow(row)
+    csv_writer =  csv.writer(open(r"//192.168.1.38/2. DATOS FUENTE/BI/Extracciones Web/exchange_rate_fix.csv", "w"), delimiter=",", lineterminator='\n')
+
+    count = 0
+    header =['Fecha','Parasolventarobligaciones']
+    csv_writer.writerow(header)
+    for row_change_rate_fix in change_rate_response["bmx"]["series"][0]['datos']:
+        row = [row_change_rate_fix['fecha'],row_change_rate_fix['dato']]
+        print(row)
+        csv_writer.writerow(row)
+
+except Exception as e:
+    print(e)
+
 
     
 
