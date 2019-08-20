8ec5f717c825bfbb71a107e8c184b68adfc95c54
jesusbriod <jesusbriod@hotmail.com>
2019-08-20 15:24:55 -0500

    First Commit

--- dollar_spot_exchange_rate/src/daily_exchange_rate.py
+++ dollar_spot_exchange_rate/src/daily_exchange_rate.py
@@ -4,7 +4,7 @@ import json
 
 #Resquest to banxico's api change rate fix
 try:
-    response = requests.get('https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF43718/datos', 
+    response = requests.get('https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF60653/datos', 
     headers={
         'Bmx-Token': '4dd474b2ab133983dd703942c7b08f114db49aae7afa6fe4e1ae1d01803ea728'
     })
@@ -21,7 +21,7 @@ try:
 
     #Create the csv writer object
 
-    csv_writer =  csv.writer(open(r"//192.168.1.38/2. DATOS FUENTE/BI/Extracciones Web/exchange_rate_fix.csv", "w"), delimiter=",", lineterminator='\n')
+    csv_writer =  csv.writer(open(r"//192.168.1.38/2. DATOS FUENTE/BI/Extracciones Web/daily_exchange_rate.csv", "w"), delimiter=",", lineterminator='\n')
 
     count = 0
     header =['Fecha','Parasolventarobligaciones']
